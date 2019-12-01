cfdc529140e9d4d6e8b307cb943929e1fe3318ae
Dmitry Vorontsov <vorontsof@mail.ru>
2019-12-01 20:38:16 +0400

    :tada:xdebug setup

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo '<h2>Welcome to PHP_1 Course</h2>';
+echo '<h3>var_dump function testing ...</h3>';
+$a = [1, 3, 23, 'true', [3, 5]];
+$b = false;
+$c = 3.14;
+var_dump($a, $b, "стоп");
+var_dump("stop");
+die();
