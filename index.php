4a55b30aed8717a4f2878d3d18214e23e359d718
Dmitry Vorontsov <vorontsof@mail.ru>
2019-12-01 21:24:52 +0400

    arrays intro

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 echo '<h2>Welcome to PHP_1 Course</h2>';
 echo '<h3>var_dump function testing ...</h3>';
-$a = [1, 3, 23, 'true', [3, 5]];
-$b = false;
-$c = 3.14;
-var_dump($a, $b, "стоп");
+$a = [1, 3.14, NULL, 'sample', true, [3, 'text', false]];
+$b = [
+	['Smith' => ['name' => 'ivan', 'age' => 25, 'sex' => 'male']],
+	['Peters' => ['name' => 'peter', 'age' => 40, 'sex' => 'male']],
+	['Chisholm' => ['name' => 'sarah', 'age' => 60, 'sex' => 'female']],
+];
+var_dump($a);
+var_dump($b, "стоп");
 var_dump("stop");
 die();
