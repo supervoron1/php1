af072a0e7a52574007c48007752496f136c286d9
Dmitry Vorontsov <vorontsof@mail.ru>
2019-12-28 20:48:54 +0400

    :tada: tunes

--- lesson8/views/news.php
+++ lesson8/views/news.php
@@ -1,6 +1,6 @@
 <h2>Новости</h2>
 <? foreach ($news as $item): ?>
-    <p><a href="/newspage/<?= $item['id'] ?>"><?= $item['prev'] ?></a></p>
+    <p><a href="/newspage/<?= $item['id'] ?>"><?= $item['prev'] ?>...</a></p>
 <? endforeach; ?>
 
 
